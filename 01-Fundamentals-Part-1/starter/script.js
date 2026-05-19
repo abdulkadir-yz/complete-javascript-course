390e5c3f18b4e4481daf94a1c632e20cec9e36bf
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-05-19 22:26:19 +0200

    Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,15 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(js);
+// console.log(40 + 8 + 23 - 10);
+// for (let i = 0; i < 50; i++) {
+//     console.log(i);
+// }
+// for (let i = 0; i < 10; i++) {
+//
+// }
+// const myArray  = [1, 2, 3, 4, 5];
+// console.log(myArray.slice(0, 2));
+// console.log(myArray.sli())
+
+// const hello = ' hello from seai#007';
+// console.log(hello.split(' ').slice(2, 4));
