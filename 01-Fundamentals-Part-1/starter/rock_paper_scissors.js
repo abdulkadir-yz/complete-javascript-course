f8b0227545cbda549e0072ea28592eb513177f7f
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-05-29 01:44:11 +0200

    Update

---
+++ 01-Fundamentals-Part-1/starter/rock_paper_scissors.js
@@ -0,0 +1,28 @@
+
+const readline = require("readline"); // readline module for user input, we can read input with this,
+                                      // require it at the top of the file, and then create an interface to read from stdin and write to stdout.
+
+
+const choices = ["rock", "paper", "scissors"];
+const computer = choices[Math.floor(Math.random() * 3)]; // Math.random() generates a random number between 0 (inclusive) and 1 (exclusive).
+                                                                  // By multiplying it by 3, we get a number between 0 and 3 (exclusive).
+                                                                  //Math.floor() then rounds it down to the nearest whole number,
+                                                                   // giving us an index of 0, 1, or 2, which corresponds to "rock", "paper", or "scissors" in the choices array.
+
+const rl = readline.createInterface({  // in here we create Interface , process.stdin = listening to user input, process.stdout = writing output to console
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("Your choice (rock - paper - scissors): ", (player) => {
+  rl.close();
+
+  console.log(`You: ${player} | Computer: ${computer}`);
+
+  if (player === computer) console.log("One more time!");
+  else if (
+      (player === "rock"     && computer === "scissors") ||
+      (player === "scissors" && computer === "paper")    ||
+      (player === "paper"    && computer === "rock")
+  ) console.log("You win!");
+  else console.log("Computer wins!");
+});
