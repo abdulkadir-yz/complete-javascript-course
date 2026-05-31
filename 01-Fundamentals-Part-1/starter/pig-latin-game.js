3a2afc24789d32abfa2e9abfaca528c8ca79b6e4
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-05-31 22:25:00 +0200

    Update

---
+++ 01-Fundamentals-Part-1/starter/pig-latin-game.js
@@ -0,0 +1,12 @@
+const vowels = ["a", "e", "i", "o", "u"];
+
+function pigLatin(word) {
+    const firstLetter = word[0].toLowerCase();
+    if (vowels.includes(firstLetter)) {
+        return word + "yay";
+    } else {
+        return word.slice(1) + firstLetter + "ay";
+    }
+}
+
+console.log(pigLatin("apple"));
